930580038fb990d8662c907f27eac2d834dfcfe2
Mamithi <19lawrence93@gmail.com>
2018-01-29 22:26:33 +0300

    Saving data to an external storage

--- app/src/main/java/com/mamithi/savefiles/MainActivity.java
+++ app/src/main/java/com/mamithi/savefiles/MainActivity.java
@@ -1,12 +1,19 @@
 package com.mamithi.savefiles;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -16,8 +23,14 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
 
     private static final String FILE_NAME = "example.txt";
+    private static final String LOG_TAG = "";
 
     EditText mEditText;
+
+    // Data to be saved externally
+    EditText fileName;
+    EditText text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mEditText = findViewById(R.id.edit_text);
+
+        fileName = findViewById(R.id.fileName);
+        text = findViewById(R.id.text);
     }
 
     public void save(View v){
@@ -82,4 +98,61 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    // Checks if external storage is available for read and write
+    public boolean isExternalStorageWritable(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    // Checks if external storage is available to at least read
+    public boolean isExternalStorageReadable(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)){
+            return true;
+        }
+        return false;
+    }
+
+    public void writeFile(View v){
+        if(isExternalStorageWritable() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            File textFile = new File(Environment.getExternalStorageDirectory(), fileName.getText().toString());
+
+            try {
+                FileOutputStream fos = new FileOutputStream(textFile);
+                fos.write(text.getText().toString().getBytes());
+                fos.close();
+
+                Toast.makeText(this, "File saved.", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Toast.makeText(this, "Cannot write to Extenal storage", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkPermission(String permission){
+       int check = ContextCompat.checkSelfPermission(this, permission);
+       return (check == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public File getAlbumStorageDir(String albumName, View v){
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+
+        if(!file.mkdirs()){
+            Log.e(LOG_TAG, "Directory not created");
+        }
+
+        return file;
+    }
+
 }
